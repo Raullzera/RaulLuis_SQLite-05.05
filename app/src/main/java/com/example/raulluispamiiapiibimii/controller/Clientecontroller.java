ceac57ab6e6e93ebf8966deb489a5cb474262665
billafranca <bragap379@gmail.com>
2026-06-02 11:25:48 -0300

    Commitando mesmo com erro

--- app/src/main/java/com/example/raulluispamiiapiibimii/controller/Clientecontroller.java
+++ app/src/main/java/com/example/raulluispamiiapiibimii/controller/Clientecontroller.java
@@ -1,12 +1,26 @@
 package com.example.raulluispamiiapiibimii.controller;
 
+import android.content.ContentValues;
 import android.content.Context;
 
+import com.example.raulluispamiiapiibimii.datamodel.ClienteDataModel;
 import com.example.raulluispamiiapiibimii.datasource.AppDataBase;
+import com.example.raulluispamiiapiibimii.model.Cliente;
 import com.example.raulluispamiiapiibimii.view.MainActivity;
 
-public class Clientecontroller extends AppDataBase {
+public class Clientecontroller extends AppDataBase implements iCRUD<Cliente> {
         public Clientecontroller(Context context){
+
             super(context);
+        }
+
+        public boolean inserir(Cliente obj){
+            dados = new ContentValues();
+            dados.put(ClienteDataModel.NOME, obj.getNome());
+            dados.put(ClienteDataModel.EMAIL, obj.getEmail());
+
+            String tabela = ClienteDataModel.TABELA;
+            return insert(tabela, dados);
+
         }
 }
